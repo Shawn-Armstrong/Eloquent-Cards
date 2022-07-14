6e4af16d584565a3c8cb65290e7de181709e6769
Siriein <jenniferlin124@gmail.com>
2022-07-14 11:05:30 -0700

    added Steven's Sidebar code

---
+++ Client/src/views/FormView.vue
@@ -0,0 +1,26 @@
+<template>
+  <Shawn_Form />
+</template>
+
+<script>
+import Shawn_Form from '../components/Shawn_Form.vue';
+export default {
+  name: 'App',
+  components: { Shawn_Form },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
